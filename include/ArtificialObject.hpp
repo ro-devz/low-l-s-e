510d94182d26c519ec2c66888bfc1f61ff8d6faa
ro-devz <148788571+ro-devz@users.noreply.github.com>
2024-12-20 09:28:56 +0100

    Update ArtificialObject.hpp

--- include/ArtificialObject.hpp
+++ include/ArtificialObject.hpp
@@ -5,31 +5,26 @@
 // Copyright   : Your copyright notice
 // Description : Final Project
 //============================================================================
-
-
 #ifndef ARTIFICIAL_OBJECT_HPP
 #define ARTIFICIAL_OBJECT_HPP
 
 #include "SpaceObject.hpp"
-#include <string>
-
-using namespace std;
 
 class ArtificialObject : public SpaceObject {
 protected:
-    float width, height;  // in (m)
-    float thrustCapacity; // Thrust capacity in N
-    // An artificial object shall be represented as a rectangle
+    float width, height;      // Dimensions in meters
+    float thrustCapacity;     // Thrust capacity in Newtons
 
 public:
-
-    ArtificialObject(const string &name, float x, float y, float vx, float vy, float mass, const string &color, float width, float height, float thrustCapacity);
+    ArtificialObject(const string &name, float x, float y, float vx, float vy,
+                    float mass, const string &color, float width, float height, 
+                    float thrustCapacity);
     ~ArtificialObject() override = default;
-    void update(double timeStep, const vector<SpaceObject*>& allObjects) override;
 
-    void render() const override;
+    float getWidth() const { return width; }
+    float getHeight() const { return height; }
+    void render(sf::RenderWindow& window, float scale, const sf::Vector2f& center) const override;
     void applyThrust(double dx, double dy);
-
 };
 
 #endif
