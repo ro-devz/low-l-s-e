dfd27ec04a37e787153178e975173160ba1b2ee0
ro-devz <148788571+ro-devz@users.noreply.github.com>
2024-12-20 09:28:41 +0100

    Update StellarObject.hpp

--- include/StellarObject.hpp
+++ include/StellarObject.hpp
@@ -5,32 +5,22 @@
 // Copyright   : Your copyright notice
 // Description : Final Project
 //============================================================================
-
-
 #ifndef STELLAR_OBJECT_HPP
 #define STELLAR_OBJECT_HPP
 
 #include "SpaceObject.hpp"
-#include <string>
-
-using namespace std;
 
 class StellarObject : public SpaceObject {
 protected:
-    float radius;  // Radius of the stellar object
-    float thrustCapacity; // Thrust capacity in Newtons
-
-    // A stellar object shall be represented as a circle
+    float radius;  // Radius of the stellar object in meters
 
 public:
-
-    StellarObject(const string &name, float x, float y, float vx, float vy, float mass, const string &color, float radius);  // Use const std::string&
+    StellarObject(const string &name, float x, float y, float vx, float vy, 
+                  float mass, const string &color, float radius);
     ~StellarObject() override = default;
 
+    float getRadius() const { return radius; }
     void render(sf::RenderWindow& window, float scale, const sf::Vector2f& center) const override;
-    
-    // Add method to apply thrust
-    void applyThrust(float thrustX, float thrustY, float timeStep);
 };
 
 #endif
